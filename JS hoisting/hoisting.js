06618053a91a59aa9bb1ff2fb0256f58ac53fada
PrasannaSelva <prasanna.ap1298@gmail.com>
2021-07-06 15:49:22 +0530

    ek

---
+++ JS hoisting/hoisting.js
@@ -0,0 +1,26 @@
+ 
+//a=10;
+//var a;
+//a=10;
+//var a=10;
+ console.log(a);
+var a;
+//a=10;
+var a=10;
+console.log(a);
+
+
+
+
+x= a+a;
+var x;
+
+
+//a=10;
+//let a;
+//a=10;
+//let a=10;
+//console.log(a);
+//let a;
+//a=10;
+//let a=10;
